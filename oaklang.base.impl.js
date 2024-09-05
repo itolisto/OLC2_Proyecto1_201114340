89b9833902a3e2609cacadbdc9ca8b132ab2014f
Mauricio Flores <itolisto@gmail.com>
2024-09-05 00:28:01 -0600

    feat: Crear node y visitor base

---
+++ oaklang.base.impl.js
@@ -0,0 +1,57 @@
+/**
+ * @typedef {Object} Location
+ * @property {Object} start
+ * @property {number} start.offset
+ * @property {number} start.line
+ * @property {number} start.column
+ * @property {Object} end
+ * @property {number} end.offset
+ * @property {number} end.line
+ * @property {number} end.column
+ */
+
+/**
+ * @typedef {Object} BaseVisitor
+ */
+export class BaseVisitor {
+    /**
+     * Implementar numero
+     * 
+     * @method
+     * @name BaseVisitor#visitNumero
+     * @param {BaseNode} node Nodo a visitar
+     * @throws {Error} Clase base no está implementada
+     */
+    visitNumero(node) {
+        throw new Error("Sin implementar");
+    }
+}
+
+/**
+ * @typedef {Object} BaseNode Clase base para cualquier expresión
+ * @property {Location} location Ubicación del nodo en el código fuente
+ */
+export class BaseNode {
+    /**
+     * @param {Location} location Ubicación del nodo en el código fuente
+     */
+    constructor(location) {
+        /**
+         * Ubicación del nodo en el código fuente
+         * @type {Location}
+         * @public
+         */
+        this.location = location;
+    }
+
+    /**
+     * Procesar nodo utilizando visitor
+     * @method
+     * @name BaseNode#accept
+     * @param {BaseVisitor} visitor Implementación de visitor
+     * @throws {Error} Clase base no está implementada
+     */
+    accept(visitor) {
+        throw new Error("Sin implementar");
+    }
+}
