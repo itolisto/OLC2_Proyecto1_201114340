e08515e0546eee3b30956598d2946ab8dd090bd8
Mauricio Flores <itolisto@gmail.com>
2024-09-08 18:15:41 -0600

    feat: Empezar con acciones semanticas

--- oaklang.nodes.impl.js
+++ oaklang.nodes.impl.js
@@ -1,16 +1,5 @@
-import { BaseNode } from "./oaklang.base.impl.js";
-
-export class NumberNode extends BaseNode {
-
-    constructor(location, props) {
-        super(location);
-        this.props = props;
-    }
-
-    /**
-     * @type {BaseNode['accept']}
-     */
-    accept(visitor) {
-        return visitor.visitNumero(this);
+class Statement {
+    constructor() {
+        this.location
     }
 }
