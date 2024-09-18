76954cad08ab4c1881d13b0e5dd346752c1a1bee
Mauricio Flores <itolisto@gmail.com>
2024-09-17 21:07:54 -0600

    chore: Agregar jdocs a instancia

--- instance.js
+++ instance.js
@@ -1,17 +1,39 @@
+/**
+ * Clase que representa una instancia de una clase en OakLang.
+ * Permite gestionar propiedades y valores asociados a una instancia de clase.
+ */
 export class Instance {
+    /**
+     * Crea una nueva instancia de una clase OakLang.
+     * 
+     * @param {OakClass} oakClass - La clase OakLang a la que pertenece esta instancia.
+     * @param {string} type - El tipo de la instancia (nombre de la clase).
+     */
     constructor(oakClass, type) {
-        // type OakClass
+        /** @type {OakClass} */
         this.oakClass = oakClass
-        // type
+        /** @type {string} */
         this.type = type
+        /** @type {Array} */
         this.properties = []
-
     }
 
+    /**
+     * Establece el valor de una propiedad en la instancia.
+     * 
+     * @param {string} name - El nombre de la propiedad que se va a establecer.
+     * @param {*} value - El valor que se asignará a la propiedad.
+     */
     set(name, value) {
         this.properties[name] = value
     }
 
+    /**
+     * Recupera el valor de una propiedad de la instancia.
+     * 
+     * @param {string} name - El nombre de la propiedad que se desea obtener.
+     * @returns {*} El valor de la propiedad o `undefined` si no existe.
+     */
     getProperty(name) {
         return this.properties[name]
     }
